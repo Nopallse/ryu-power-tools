3f17fb649a88eba65d636d3bfe02c924de66316b
Hylxnn <zafranamuri11@gmail.com>
2026-09-04 23:05:38 +0700

    Feat : changing scale zoom

--- fe/app/[lang]/product/[id]/page.tsx
+++ fe/app/[lang]/product/[id]/page.tsx
@@ -251,7 +251,7 @@ export default function ProductDetailPage() {
                     const x = ((e.clientX - rect.left) / rect.width) * 100;
                     const y = ((e.clientY - rect.top) / rect.height) * 100;
                     img.style.transformOrigin = `${x}% ${y}%`;
-                    img.style.transform = "scale(2)";
+                    img.style.transform = "scale(1.1)";
                   }}
                   onMouseLeave={(e) => {
                     const img = e.currentTarget;
